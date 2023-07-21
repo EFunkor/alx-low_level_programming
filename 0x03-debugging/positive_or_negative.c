4312b11140c91cbb338658a2352bab325ac7a281
EFunkor <elsiefunk12@gmail.com>
2023-07-21 00:40:43 -0700

    update

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,12 +1,36 @@
-#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
 
+/* betty style doc for function main goes there */
+/**
+ * main - Assigns a random number to n, and then declares
+ *
+ * Description: Assigns a random number to n,
+ * And then declares if it is postive, 0 or negative.
+ *
+ * Return: 0 for no error once done
+ */
 void positive_or_negative(int i)
 {
-        if(i > 0)
-                printf("%d is positive\n", i);
-        else if (i < 0)
-                printf("%d is negative\n", i);
+        /*int n;*/
 
-        else
+        srand(time(0));
+        i = rand() - RAND_MAX / 2;
+        /* your code goes there */
+        if (i > 0)
+        {
+                printf("%d is positive\n", i);
+        }
+        else if (i == 0)
+        {
                 printf("%d is zero\n", i);
+        }
+        else
+        {
+                printf("%d is negative\n", i);
+        }
+
+        /*return (0);*/
 }
