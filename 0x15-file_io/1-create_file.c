7943e211790ca1460c00d055f10c3b6c01298ad0
Elsie Funkor <elsiefunk12@gmail.com>
2023-10-02 13:38:04 -0700

    tasks 1 on create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,11 @@
 #include "main.h"
 #include <unistd.h>
 #include <stdio.h>
-
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * create_file - fxn to Create a file with spec. content in it.
@@ -11,27 +15,23 @@
  * Return: For when the function fails -1 (failure).
  *         else, 1 (success).
  */
+
 int create_file(const char *filename, char *text_content)
 {
-	int FDr, wr, text_size = 0;
+	int nwf; /* New file rep */
+	int z; /* for size rep */
 
 	if (filename == NULL)
 		return (-1);
-
+	nwf = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (nwf < 0)
+		return (-1);
 	if (text_content != NULL)
 	{
-		for (text_size = 0; text_content[text_size];)
-			text_size++;
+		z = strlen(text_content);
+		nwf = write(nwf, text_content, z);
+		if (nwf < 0)
+			return (-1);
 	}
-
-/*  0600 permissions,to open a file, create as well as write, with */
-	FDr = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	write_result = write(file_descriptor, text_content, text_size);
-
-	if (FDr == -1 || wr == -1)
-		return (-1);
-
-	close(FDr);
-
 	return (1);
 }
