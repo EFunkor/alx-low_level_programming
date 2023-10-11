dbe7d1f840fb970a9b69d06f57c9456cb0a27f4c
Elsie Funkor <elsiefunk12@gmail.com>
2023-10-11 08:36:39 -0700

    print binary file

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,15 +1,17 @@
-#include "main.h"
-
+#include <stdio.h>
 
 /**
- * print_binary - An fxn that will print the Binary rep ofa number.
- * @n: no to converted to binary
+ * print_binary - fxn that will print the binary rep, of a nbr.
+ * @n: no. to be printed when changed to bin.
  */
 
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
-		print_binary(n >> 1);
 
-	_putchar((n & 1) + '0');
+	if (n > 1)
+		print_binary(
+		    n >> 1); /* move n by 1 to process the next bit */
+
+
+	putchar((n & 1) + '0');
 }
