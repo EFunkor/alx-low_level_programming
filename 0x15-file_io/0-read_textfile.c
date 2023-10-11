c104131743d9aa68a3344f5247c0b29d08cdd931
Elsie Funkor <elsiefunk12@gmail.com>
2023-10-11 12:50:59 -0700

    task 0 for file input output updated

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,37 +1,51 @@
 #include "main.h"
 
-/**
- * read_textfile - A function that reads a text file and prints to POSIX stdout
- * @letters: number of letters it will read and print.
- * @filename: name of the file being read or opened.
- * Return: nL, actual number of letters it could read and print or 0 on error.
+/*
+ * read_textfile - read, then print a text file.
+ * @filename: Name of  file to be read
+ * @letters: Nbr of letters to print.
+ *
+ * Return: The nbr of letters printed. Returns 0, failure.
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	/* list of Variables that are been declared */
-	char *buffer; /* buff for read data stored */
-	ssize_t fldesc; /* file Descriptor */
-	ssize_t NL;  /* bytes nbr written to STDOUT */
-	ssize_t Z; /* nbr of bytes that are read in that file */
+	int file_descriptor;
+	ssize_t bytes_read, bytes_written;
+	char *buffer;
 
+	if (filename == NULL)
+		return (0);
 
-	fldesc = open(filename, O_RDONLY);
-	if (fldesc == -1)
-		return (0); /* 0 if file wont open */
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
+		return (0);
 
-	/* buffer memory alloc described */
 	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(file_descriptor);
+		return (0);
+	}
 
-	/* read data from file into the buffer */
-	Z = read(fldesc, buffer, letters);
+	bytes_read = read(file_descriptor, buffer, letters);
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(file_descriptor);
+		return (0);
+	}
 
-	/* to write the read data for the STDOUT */
-	NL = write(STDOUT_FILENO, buffer, Z);
-
-	/* free allocated memmory */
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
 	free(buffer);
-	close(fldesc);
+		close(file_descriptor);
+		return (0);
+	}
 
-	return (NL);
-}
+	free(buffer);
+	close(file_descriptor);
+
+	return (bytes_written);
+}  
